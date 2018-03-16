f47ce96f8db6fcdb0bc60d405abbd23b40c3ffe8
Tiago Wolker Leite <tiago.wolker.leite@gmail.com>
2018-03-15 21:53:13 -0300

    [POO] [2018-01] Começo do exercício 2 (15/03)

--- 2018-03-15/0315Exercicio01/src/pkg0315exercicio01/Main.java
+++ 2018-03-15/0315Exercicio01/src/pkg0315exercicio01/Main.java
@@ -11,6 +11,11 @@ import java.util.Scanner;
  * @author <a href="mailto:tiago.wolker.leite@gmail.com">Tiago Wolker Leite</a>
  */
 public class Main {
+        
+    private static final float PESO1 = 2;
+    private static final float PESO2 = 3;
+    private static final float PESO3 = 5;
+    private static final float PESO_TOTAL = PESO1 + PESO2 + PESO3;
     
     /**
      * Método principal.
@@ -18,11 +23,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        final float PESO1 = 2;
-        final float PESO2 = 3;
-        final float PESO3 = 5;
-        final float PESO_TOTAL = PESO1 + PESO2 + PESO3;
         
         float nota1;
         float nota2;
@@ -38,11 +38,23 @@ public class Main {
         System.out.println("Digite a terceira nota: ");
         nota3 = ler.nextFloat();
         
-        mediaFinal = ((nota1 * PESO1) + (nota2 * PESO2) + (nota3 * PESO3))
-                / PESO_TOTAL;
+        mediaFinal = calcularMedia(nota1, nota2, nota3);
         
         System.out.println("Média final: " + mediaFinal);
         
+    }
+
+    /**
+     * Método que calcula a média ponderada para as notas informadas.
+     *
+     * @param nota1 Valor da primeira nota.
+     * @param nota2 Valor da segunda nota.
+     * @param nota3 Valor da terceira nota.
+     * @return Média ponderada.
+     */
+    private static float calcularMedia(float nota1, float nota2, float nota3) {
+        return ((nota1 * PESO1) + (nota2 * PESO2) + (nota3 * PESO3))
+                / PESO_TOTAL;
     }
     
 }
